064ed1fbfb406af7934cad8071073ca0a50645be
Leonardo Lazaro Barboza <leonardo_laz@outlook.com>
2022-12-05 13:01:57 -0300

    Update script.js

--- Verificador de Idade/script.js
+++ Verificador de Idade/script.js
@@ -13,11 +13,29 @@ function verificar() {
         img.setAttribute('id', 'foto')
         if (sexo[0].checked) {
             genero = 'homem'
-            if (idade >= 0 && idade < 10) {}
+            if (idade >= 0 && idade < 10) {
+                img.setAttribute('src', 'Fotos/foto-bebe-m.png')
+            } else if (idade < 21) {
+                img.setAttribute('src', 'Fotos/foto-jovem-m.png')
+            } else if (idade < 50) {
+                img.setAttribute('src', 'Fotos/foto-adulto-m.png')
+            } else {
+                img.setAttribute('src', 'Fotos/foto-idoso-m.png')
+            }
         } else {
             genero = 'mulher'
+            if (idade >= 0 && idade < 10) {
+                img.setAttribute('src', 'Fotos/foto-bebe-f.png')
+            } else if (idade < 21) {
+                img.setAttribute('src', 'Fotos/foto-jovem-f.png')
+            } else if (idade < 50) {
+                img.setAttribute('src', 'Fotos/foto-adulto-f.png')
+            } else {
+                img.setAttribute('src', 'Fotos/foto-idoso-f.png')
+            }
         }
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }   
 }
