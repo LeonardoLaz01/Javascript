dd1f37f5a94e25b04f0a812cf606591d6d56a55b
Leonardo Lazaro Barboza <leonardo_laz@outlook.com>
2022-12-13 21:25:19 -0300

    Update scripts.js

--- CursoEmVideo/Exercícios/Nível 2 - Condições em JavaScript/scripts.js
+++ CursoEmVideo/Exercícios/Nível 2 - Condições em JavaScript/scripts.js
@@ -88,4 +88,24 @@ O que vamos fazer?
     } else {
         res.innerHTML += `OPÇÃO INVÁLIDA! Você digitou ${valor1} e ${valor2}, mas não sei o que fazer com eles.`
     }
+} function estacao() {
+    var mes = window.prompt('Digite o mês em extenso (ex: Setembro)')
+    var estacao
+    switch (mes.toUpperCase()) {
+        case 'JANEIRO': case 'FEVEREIRO': case 'MARÇO':
+            estacao = 'INVERNO'
+            break
+        case 'ABRIL': case 'MAIO': case 'JUNHO':
+            estacao = 'PRIMAVERA'
+            break
+        case 'JULHO': case 'AGOSTO': case 'SETEMBRO':
+            estacao = 'VERÃO'
+            break
+        case 'OUTUBRO': case 'NOVEMBRO': case 'DEZEMBRO':
+            estacao = 'OUTONO'
+            break
+        default:
+            estacao = 'INDEFINIDA'
+            break
+    } res.innerHTML = `<p>No mês de <mark>${mes}</mark>, estamos na estação <mark><strong>${estacao}</strong></mark>.</p>`
 }
