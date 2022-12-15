c152f3ecfa479f9b0ede48c28fb69e2c68165f5e
Leonardo Lazaro Barboza <113008406+LeonardoLaz01@users.noreply.github.com>
2022-12-15 17:02:35 -0300

    Update script.js

--- Super Contador/script.js
+++ Super Contador/script.js
@@ -1,9 +1,9 @@
 function contador() {
-    var res = document.getElementById('res');
-    res.innerHTML = '<p></p>';
-    var inicio = document.getElementById('inicio').value;
-    var fim = document.getElementById('fim').value;
-    var passo = document.getElementById('passo').value;
+    var res = document.getElementById('res')
+    res.innerHTML = '<p></p>'
+    var inicio = document.getElementById('inicio').value
+    var fim = document.getElementById('fim').value
+    var passo = document.getElementById('passo').value
     if (passo == 0) {
         window.alert('Passo inválido! Considerando PASSO 1')
         passo = 1
