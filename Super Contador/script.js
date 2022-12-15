af17b8576a70b322948b6580c758e7123cfa3aaa
Leonardo Lazaro Barboza <leonardo_laz@outlook.com>
2022-12-15 16:13:00 -0300

    Create script.js

---
+++ Super Contador/script.js
@@ -0,0 +1,19 @@
+function contador() {
+    var res = document.getElementById('res');
+    res.innerHTML = '<p></p>';
+    var inicio = document.getElementById('inicio').value;
+    var fim = document.getElementById('fim').value;
+    var passo = document.getElementById('passo').value;
+    if (passo == 0) {
+        window.alert('Passo inválido! Considerando PASSO 1')
+        passo = 1
+    }
+    if (inicio != null) {
+        res.innerHTML = '<br>Contando:<br>';
+        for (var c = Number(inicio); c <= Number(fim); c += Number(passo)) {
+         res.innerHTML += `${c} &#128073`;
+        } res.innerHTML += `&#127988`;
+    } else {
+        res.innerHTML = '<p>Impossível contar!</p>'
+    }
+}
