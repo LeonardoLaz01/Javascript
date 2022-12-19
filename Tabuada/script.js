5067be0ee0479cd23249dad995206081a9854cad
Leonardo Lazaro Barboza <leonardo_laz@outlook.com>
2022-12-19 16:30:57 -0300

    Update script.js

--- Tabuada/script.js
+++ Tabuada/script.js
@@ -1,12 +1,12 @@
 function tabuada() {
-    var numero = document.getElementById('numero').value
-    var res = document.getElementById('res')
-    if (numero != '') {
-        res.innerHTML = ''
-        for (c = 1; c <= 10; c++) {
-            res.innerHTML += `<p>${Number(numero)} x ${Number(c)} = ${Number(numero * c)}</p>`
-        }
-    } else {
+    let num = document.getElementById('numero').value
+    let tab = document.getElementById('tab')
+    if (num == '') {
         window.alert('erro')
+    } else {
+        tab.innerHTML = ''
+        for (let c = 1; c <= 10; c++) {
+            tab.innerHTML += `<option value="tab${c}">${num} x ${c} = ${num * c}</option>`
+        }
     }
 }
