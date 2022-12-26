4f11bb5501f67c482c804eb8d0817c94eee8f20c
Leonardo Lazaro Barboza <leonardo_laz@outlook.com>
2022-12-26 20:05:17 -0300

    Create script.js

---
+++ Analisador de Números/script.js
@@ -0,0 +1,18 @@
+const lista = []
+var tab = document.getElementById('tab')
+var soma = 0
+var res = document.getElementById('res')
+function adicionar() {
+    res.innerHTML = ''
+    var num = document.getElementById('num').value
+    lista.push(num)
+    tab.innerHTML += `<option value="tab${num}">Valor ${num} adicionado.</option>`
+} function finalizar() {
+    for (let x in lista) {soma += Number(lista[x])}
+    res.innerHTML += `<p>Ao todo, temos ${lista.length} números cadastrados.</p>`
+    res.innerHTML += `<p>O maior valor informado foi ${Math.max(...lista)}</p>`
+    res.innerHTML += `<p>O menor valor informado foi ${Math.min(...lista)}</p>`
+    res.innerHTML += `<p>Somando todos os valores, temos ${soma}</p>`
+    res.innerHTML += `<p>A média dos valores digitados é ${soma / lista.length}</p>`
+
+}
