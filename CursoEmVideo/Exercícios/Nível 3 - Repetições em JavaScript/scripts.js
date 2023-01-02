e4fb48cf070f0cd5466ec3e68f7bce084542c5c1
Leonardo Lazaro Barboza <leonardo_laz@outlook.com>
2023-01-02 03:02:59 -0300

    Update scripts.js

--- CursoEmVideo/Exercícios/Nível 3 - Repetições em JavaScript/scripts.js
+++ CursoEmVideo/Exercícios/Nível 3 - Repetições em JavaScript/scripts.js
@@ -1,8 +1,19 @@
+var res = document.getElementById('res')
 function contar() {
-    let res = document.querySelector('div#res')
     res.innerHTML = '<h2>Contando de 1 até 10</h2>'
     for (let c = 1; c <= 10; c++) {
         res.innerHTML += `${c}  &#128073; `
     }
     res.innerHTML += '&#127937;'
 }
+function contagem() {
+    res.innerHTML += '<h2>Contando de 1 até 10, marcando os pares</h2>'
+    for(let c = 1; c <= 10; c++) {
+        if (c % 2 == 0) {
+            res.innerHTML += `<mark><strong>${c}</strong></mark> &#128073; `
+        } else {
+            res.innerHTML += `${c} &#128073; `
+        }
+    }
+    res.innerHTML += '&#127937;'
+}   
